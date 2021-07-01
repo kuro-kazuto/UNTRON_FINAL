fd0bf0ba0d8778438941ad4d65737f1ef0b6388a
kuro-kazuto <galihajipabudi@gmail.com>
2021-07-01 22:17:54 +0700

    untron_v1.5

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
-import LandingPages from "./pages/LandingPages";
-import About from "./pages/About";
-import Help from "./pages/Help";
-import News from "./pages/News";
+import About from './pages/About';
+import Help from './pages/Help';
+import News from './pages/News';
+import LandingPages from './pages/LandingPages';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import logo from "./UNTRON.png";
+import { Navbar } from "react-bootstrap";
+import { Nav } from "react-bootstrap";
+import { Form } from "react-bootstrap";
+import { FormControl } from "react-bootstrap";
+import { NavDropdown } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+//lib icon
+import { HouseFill } from 'react-bootstrap-icons';
+import { InfoSquareFill } from 'react-bootstrap-icons';
+import { QuestionSquareFill } from 'react-bootstrap-icons';
+import { PersonFill } from 'react-bootstrap-icons';
+import { Search } from 'react-bootstrap-icons';
+
 function App() {
   return (
     <Router>
-      <Route exact path="/" component={LandingPages}></Route>
-      <Route path="/about" component={About}></Route>
-      <Route path="/help" component={Help}></Route>
-      <Route path="/news" component={News}></Route>
-    </Router>
+        <div>
+          <Switch>
+              <Route exact path='/' component={LandingPages} />
+              <Route path='/help' component={Help} />
+              <Route path='/about' component={About} />
+              <Route path='/news' component={News} />
+          </Switch>
+        </div>
+      </Router>
   );
 }
 
